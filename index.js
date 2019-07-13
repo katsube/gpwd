44ceaa3f2286ee2e31a9a508a1d3fab42c54d4fe
Makito Katsube <katsubemakito@gmail.com>
2019-07-13 21:54:07 +0900

    implements -b,--base option  #5

--- index.js
+++ index.js
@@ -13,6 +13,8 @@
 //--------------------------------------
 const MIN_ITEM = 1;
 const MAX_ITEM = 65534;
+const MIN_BASE_LEN = 2;
+const MAX_BASE_LEN = 64;
 
 //--------------------------------------
 // Module
@@ -27,27 +29,41 @@ const passwd  = new genPassword();
 program
   .version("1.1.0")
   .option("-l, --length [bytes]",  "string length [bytes]", 8)
-  .option("-s, --strength [mode]", "string strength [god|strong|normal|weak] and more", "strong")
   .option("-i, --item [number]",   "how many generate [number]", 1)
+  .option("-s, --strength [mode]", "string strength [god|strong|normal|weak] and more", "strong")
+  .option("-b, --base [string]",   "base charactor [string]. Higher priority than -s,--strength option")
   .parse(process.argv);
 
 //--------------------------------------
 // Validation
 //--------------------------------------
+// --length (is number)
 if( ! Number.isInteger( Number(program.length) ) ){
   error("-l, --length option is only integer.");
 }
+// --length (between min to max)
 if( ! (genPassword.MIN_LENGTH <= Number(program.length) && Number(program.length) <= genPassword.MAX_LENGTH) ){
   error(`-l, --length option is need between ${genPassword.MIN_LENGTH} to ${genPassword.MAX_LENGTH}`);
 }
+// --strength
 if( ! passwd.existsStrength(program.strength) ){
   error("-s, --strength option is [god|strong|normal|weak] and more.");
 }
+// --item (is number)
 if( ! Number.isInteger( Number(program.item) ) ){
   error("-i, --item option is only integer.");
 }
+// --item (between min to max)
 if( ! (MIN_ITEM <= Number(program.item) && Number(program.item) <= MAX_ITEM) ){
   error(`-l, --length option is need between ${MIN_ITEM} to ${MAX_ITEM}`);
+}
+// --base (charactor type)
+if( (program.base !== undefined) && ( ! program.base.match(/^[a-zA-Z0-9\.\-_\+/!\"#\$%&'\(\)\*,;<=>?@\[\]\^`{\|}~]*$/) )){
+  error("-b, --base option is only use [a-zA-Z0-9.-_+/!\"#$%&'()*,;<=>?@[]^`{|}~]");
+}
+// --base (string length)
+if( (program.base !== undefined) && !(MIN_BASE_LEN <= program.base.length && program.base.length <= MAX_BASE_LEN) ){
+  error(`-b, --base option is need between ${MIN_BASE_LEN} to ${MAX_BASE_LEN} string length`);
 }
 
 //--------------------------------------
@@ -55,7 +71,8 @@ if( ! (MIN_ITEM <= Number(program.item) && Number(program.item) <= MAX_ITEM) ){
 //--------------------------------------
 passwd.setOption({
     length: Number(program.length),
-  strength: program.strength
+  strength: program.strength,
+      base: program.base
 });
 
 for(let i=0; i<program.item; i++){

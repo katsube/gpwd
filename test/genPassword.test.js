8f7b566affa1d15bbf5ec682e30126d3b9621c9a
Makito Katsube <katsubemakito@gmail.com>
2019-07-13 22:09:48 +0900

    update test code

--- test/genPassword.test.js
+++ test/genPassword.test.js
@@ -95,29 +95,48 @@ test('password length and strength', () => {
 });
 
 test('setOption()', () => {
-  const defaultOpt = {length:8, strength:"strong"};
+  const defaultOpt = {length:8, strength:"strong", base:undefined};
 
   // check default value
   const passwd = new genPassword();
   expect( passwd.getOption() ).toMatchObject(defaultOpt);
   expect( passwd.getOption("length") ).toBe(defaultOpt.length);
   expect( passwd.getOption("strength") ).toBe(defaultOpt.strength);
+  expect( passwd.getOption("base") ).toBe(defaultOpt.base);
 
   // no argument === no change
   passwd.setOption();
   expect( passwd.getOption() ).toMatchObject(defaultOpt);
 
+  // base is visible charactor only
+  passwd.setOption({base:"\n"});
+  expect( passwd.getOption("base") ).toBe(defaultOpt.base);
+
   // change option
   passwd.setOption({length:16});
-  expect( passwd.getOption() ).toMatchObject({length:16, strength:defaultOpt.strength});
+  expect( passwd.getOption() ).toMatchObject({length:16, strength:defaultOpt.strength, base:defaultOpt.base});
   passwd.setOption({strength:"normal"});
-  expect( passwd.getOption() ).toMatchObject({length:16, strength:"normal"});
-  passwd.setOption({length:128, strength:"god"});
-  expect( passwd.getOption() ).toMatchObject({length:128, strength:"god"});
+  expect( passwd.getOption() ).toMatchObject({length:16, strength:"normal", base:defaultOpt.base});
+  passwd.setOption({base:"01"});
+  expect( passwd.getOption() ).toMatchObject({length:16, strength:"normal", base:"01"});
+  passwd.setOption({length:128, strength:"god", base:"01"});
+  expect( passwd.getOption() ).toMatchObject({length:128, strength:"god", base:"01"});
 });
 
 test('_getBasestring()', () => {
   const passwd = new genPassword();
   expect( passwd._getBasestring("normal") ).toBe("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
   expect( passwd._getBasestring("404") ).toBe(null);
+});
+
+test('-b, --base', () => {
+  const passwd = new genPassword();
+
+  passwd.setOption({base:"01"});
+  expect(passwd.gen().get()).toMatch(/^[01]*$/);
+  passwd.setOption({base:"0123456789ABCDEF"});
+  expect(passwd.gen().get()).toMatch(/^[0-9A-F]*$/);
+
+  passwd.setOption({base:"0123456789ABCDEF", strength:"god"});
+  expect(passwd.gen().get()).toMatch(/^[0-9A-F]*$/);
 });
